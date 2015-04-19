c61d6c66f42a19d329ade05c1084eff3b1171fde
martingreenberg <grmartin@seas.upenn.edu>
2015-04-19 10:59:26 -0400

    Packaged .jar with dependencies.

--- src/main/java/autolib_catalog/Catalog.java
+++ src/main/java/autolib_catalog/Catalog.java
@@ -1,19 +1,15 @@
 package autolib_catalog;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.books.Books;
-import com.google.api.services.books.Books.Volumes.Get;
 import com.google.api.services.books.Books.Volumes.List;
 import com.google.api.services.books.BooksRequestInitializer;
-import com.google.api.services.books.BooksScopes;
 import com.google.api.services.books.model.Volume;
 
-import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Random;
